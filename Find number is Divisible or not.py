62450a5af7cbcf642caa4357143d662f98d44940
BhuvaneshRs <132887722+BhuvaneshRs@users.noreply.github.com>
2023-05-11 20:54:13 +0530

    Find number is Divisible or not

---
+++ Find number is Divisible or not.py
@@ -0,0 +1,11 @@
+x=(int(input("Enter Divisible number")))
+y=(int(input("Enter Divisor number")))
+if x%y==0:
+  print(x," is Divisible by",y)
+else:
+  print(x,"is not Divisible by",y)
+
+#Output:
+Enter Divisible number65
+Enter Divisor number2
+65 is not Divisible by 2
